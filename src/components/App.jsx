4e84458412e51d9494e84ec33141bbed0e8aa3e8
Kaishiyoku <dmp@nfze.de>
2020-06-29 21:22:32 +0200

    replaced React router by Reach router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { hot } from 'react-hot-loader';
+import {hot} from 'react-hot-loader';
 import 'assets/scss/App.scss';
 import AboutPage from 'components/AboutPage';
 import WelcomePage from 'components/WelcomePage';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import {Link, Router} from "@reach/router"
 
 class App extends React.PureComponent {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="app">
-          <nav>
-            <Link to="/">Home</Link> | <Link to="/about">About</Link>
-          </nav>
-          <hr />
-          <Route exact path="/" component={WelcomePage} />
-          <Route path="/about" component={AboutPage} />
-        </div>
-      </BrowserRouter>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <nav>
+                    <Link to="/">Home</Link> | <Link to="about">About</Link>
+                </nav>
+                <hr/>
+                <Router>
+                    <WelcomePage path="/"/>
+                    <AboutPage path="about"/>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default hot(module)(App);
